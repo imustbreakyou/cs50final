5d709a6d8d21e06898f3e150aa09cbb51d8145f8
mike <8876812+imustbreakyou@users.noreply.github.com>
2023-12-13 11:20:48 -0800

    implement URL concatenation function with hardcoded parameter
    - Created function to concatenate URL with URL parameter
    - Harded Coded Spanish as a query
    - Confirmed function operates as expected with Spanish data returned
    - Built Pseudocode framework for handle_form_submission function

--- app.py
+++ app.py
@@ -2,8 +2,12 @@
 # environmental variable information from: https://ioflood.com/blog/python-get-environment-variable/#:~:text=to%20access%20it.-,Use%20os.,to%20safely%20access%20environment%20variables.#
 # calling environmental variables in python from this source: https://dagster.io/blog/python-environment-variables
 # further .env assitance from here: https://stackoverflow.com/questions/41546883/what-is-the-use-of-python-dotenv # 
+# API response assitance from: https://www.geeksforgeeks.org/response-json-python-requests/
+# Form to dict from here https://vortex.hashnode.dev/how-to-obtain-dict-from-a-flask-request-form-ck6c6ertx006s3cs1rw60rsk8 #
+#
+
 from flask import Flask, request, render_template
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 import os
 import requests
 
@@ -11,10 +15,6 @@ import requests
 
 app = Flask(__name__)
 app.debug = True
-
-print(os.environ)
-
-
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -30,17 +30,16 @@ def results():
 
 
 
-
-
-
-
-    
-
 @app.route("/call_api", methods=['GET', 'POST'])
 def call_api():
     if request.method == "POST":
         print("post request received")
-    
+
+        source_url = "https://api.twitch.tv/helix/streams"
+        url = build_api_url(source_url)
+        print(url)
+
+
         # Access API keys 
         load_dotenv('/absolute/path/to/.env')
         client_id = os.getenv('TWITH_CLIENT_ID')
@@ -50,7 +49,8 @@ def call_api():
             print("missing API token or id")
             return None
 
-        url = "https://api.twitch.tv/helix/streams"
+  
+      
 
         headers = {
             
@@ -61,9 +61,10 @@ def call_api():
         
         response = requests.get(url, headers=headers)
         print(response)
+        print(response.json())
 
         if response.status_code == 200:
-            #return repsonse.json()
+            #print response
             results = "data"
             return render_template('results.html', results=results)
         
@@ -72,6 +73,35 @@ def call_api():
             return None
     else:
         return redirect('/')
+    
+def build_api_url(source_url):
+    
+    
+    # parameter_1['value'] = en -- To use one we have handle_form function
+          # Set API call params with f"url?{}{}{}""
+        
+
+    #hard coded value
+    parameter_1_name = "language="
+    parameter_1_value = "es"
+
+    
+    final_url = source_url+"?"+parameter_1_name+parameter_1_value
+    print(final_url)
+    return final_url
+
+
+
+# Form Submission Function 
+#def handle_form_submission():
+#   data = request.form.to_dict()
+# n=0  
+# for data in data:
+#       data[f'variable_{n}'] = ""
+#       n += 1
+# return data 
+#
+#
 
     
 
